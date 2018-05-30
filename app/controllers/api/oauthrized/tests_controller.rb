f22412d1ad880eb20e308e2de7ed88140a8bcc4f
kei2100 <kei.arima.2100@gmail.com>
2018-05-30 23:05:21 +0900

    reset project

--- app/controllers/api/oauthrized/tests_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api::Oauthrized
-  class TestsController < ActionController::Base
-    before_action :doorkeeper_authorize!
-
-    def show
-      render status: :ok , json: {}
-    end
-  end
-end
--- app/controllers/jobs_controller.rb
+++
@@ -1,8 +0,0 @@
-class JobsController < ApplicationController
-  protect_from_forgery except: :perform
-
-  def perform
-    ExampleJob.perform_later
-    render plain: 'ok'
-  end
-end
