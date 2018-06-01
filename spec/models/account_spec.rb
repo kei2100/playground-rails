b1ec761973113b779368a7cd67e36720d650b313
kei2100 <kei.arima.2100@gmail.com>
2018-06-01 23:17:09 +0900

    fix spec

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  describe '#save' do
-    let(:account) { build(:account, login_id: SecureRandom.alphanumeric(16)) }
-    subject { account.save }
+  let(:account) { build(:account, login_id: SecureRandom.alphanumeric(16)) }
 
+  describe '#save' do
+    subject { account.save }
     it { is_expected.to be_truthy }
-    it {
-      subject
-      expect(Account.find_by(login_id: account.login_id)).to eq(account)
-    }
+  end
+
+  describe '#find' do
+    before do
+      account.save
+    end
+    subject { Account.find_by(login_id: account.login_id) }
+    it { is_expected.to eq(account) }
   end
 end
